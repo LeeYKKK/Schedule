38e64736c0b72d05598b795607d6ea13debf7ffc
LeeYKKK <282019754@qq.com>
2017-05-31 11:36:05 +0800

    测试提交

--- app/src/main/java/com/example/yk/schedule/activity/MainActivity.java
+++ app/src/main/java/com/example/yk/schedule/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.yk.schedule.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.yk.schedule.R;
 
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.myappwidget);
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
     }
 }
